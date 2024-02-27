12d215af1931d6ded58269c3e888f632127aa3a3
nabil <mynameisnabil09@gmail.com>
2024-02-27 13:37:53 +0700

    First Commit

---
+++ testerlib/src/main/java/com/example/testerlib/Tester.java
@@ -0,0 +1,15 @@
+package com.example.testerlib;
+
+import android.util.Log;
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void test(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+}
